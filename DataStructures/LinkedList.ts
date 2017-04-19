520d0336cf711b28d66d83ac4a9d9bc8eac51a27
Prasanna Choudhari <choulinux@gmail.com>
2017-04-20 01:25:43 +0530

    Added doubly linked list

--- DataStructures/LinkedList.ts
+++ DataStructures/LinkedList.ts
@@ -12,12 +12,12 @@ class LinkedListNode {
 
 export class LinkedList {
 
-    private head: LinkedListNode;
+    protected head: LinkedListNode;
     public constructor() {
-        
+
     }
 
-    public AddNode(data: any) {
+    public addNode(data: any) {
         var temp = new LinkedListNode(data);
         if (this.head == null) {
             this.head = temp;
@@ -33,9 +33,38 @@ export class LinkedList {
 
     public printAll() {
         var curr = this.head;
-        while (curr.next != null) {
+        while (curr != null) {
             console.log(curr.data);
             curr = curr.next;
         }
+    }
+}
+
+export class DoublyLinkedList extends LinkedList {
+
+    public addNode(data: any) {
+        var temp = new LinkedListNode(data);
+        if (this.head == null) {
+            this.head = temp;
+        }
+        else {
+            var curr = this.head;
+            while (curr.next != null) {
+                curr = curr.next;
+            }
+            curr.next = temp;
+            temp.previous = curr;
+        }
+    }
+
+    public printReverse() {
+        var curr = this.head;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        do {
+            console.log(curr.data);
+            curr = curr.previous;
+        } while (curr != null);
     }
 }
--- DataStructures/Main.ts
+++ DataStructures/Main.ts
@@ -1,18 +1,32 @@
-import { LinkedList } from "./LinkedList";
+import { LinkedList, DoublyLinkedList } from "./LinkedList";
 
 class Program {
-    public static main(){
+    public static main() {
         Program.TestLinkedList();
+        Program.TestDoublyLinkedList();
     }
 
-    private static TestLinkedList(){
+    private static TestLinkedList() {
         var list = new LinkedList();
-        list.AddNode(10);
-        list.AddNode(100);
-        list.AddNode(1000);
-        list.AddNode(100);
-        list.AddNode(10);
+        list.addNode(10);
+        list.addNode(100);
+        list.addNode(1000);
+        list.addNode(100);
+        list.addNode(10);
         list.printAll();
+    }
+
+    private static TestDoublyLinkedList() {
+        var list = new DoublyLinkedList();
+        list.addNode(10);
+        list.addNode(100);
+        list.addNode(1000);
+        list.addNode(10000);
+        list.addNode(100000);
+        console.log("Printing left to right");
+        list.printAll();
+        console.log("Printing right to left");
+        list.printReverse();
     }
 
 }
